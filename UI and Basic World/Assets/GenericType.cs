7e064c69937e9ba841d20e4165852a51ef98ab2b
jkoller90 <jmk4411@gmail.com>
2017-11-27 17:02:25 -0500

    added ui and basic world level

---
+++ UI and Basic World/Assets/GenericType.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericType<T> : MonoBehaviour
+{
+	GameObject prefab;
+	public void someFn(){
+		Instantiate (prefab, new Vector3 (0, 0, 0), Quaternion.identity);
+	}
+}
+
