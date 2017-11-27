3004ae86de4d2fd3076cd8ab7c86e4882793d5f1
yao <ynex5x@gmail.com>
2017-11-27 18:26:50 -0500

    added collision detection to duck

---
+++ Assets/showInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showInfo : MonoBehaviour {
+
+	public GameObject canvas;
+	public GameObject[] canvases;
+
+	void Start(){
+		canvases = GameObject.FindGameObjectsWithTag ("InfoTag"); 
+		canvas = GameObject.FindGameObjectWithTag ("Canvas");
+//		info = canvases [0];
+		canvas.GetComponent<CanvasGroup> ().alpha = 0f;
+	}
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "Player") {
+			canvas.GetComponent<CanvasGroup> ().alpha = 1f;
+		}
+	}
+}
