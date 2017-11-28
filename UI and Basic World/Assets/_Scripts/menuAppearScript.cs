023da63f53194e5898bc58c99ea89826fb93c72d
jkoller90 <jmk4411@gmail.com>
2017-11-28 00:10:04 -0500

    escape button closes and opens canvas

---
+++ UI and Basic World/Assets/_Scripts/menuAppearScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuAppearScript : MonoBehaviour {
+
+	public GameObject menu; // Assign in inspector
+	private bool isShowing;
+
+	void Start(){
+		menu.SetActive (false);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown("escape")) {
+			isShowing = !isShowing;
+			menu.SetActive(isShowing);
+		}
+	}
+}
